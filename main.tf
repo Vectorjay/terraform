1bfdcd25a4d01cfb9d76adaeaf561f6315282218
Vector <mac@Vector-2.local>
2026-04-06 19:43:54 +0100

    update

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aws_instance" "demo" {
 }
 
 resource "null_resource" "Configure_server"{
-  # This command will run after the instance is created, it will use Ansible to deploy Docker on the instance. Make sure to have Ansible installed and configured on your local machine.
+  # This command will run after the instance is created, it will use Ansible to deploy Docker on the instance. Make sure to have Ansible installed and configured on your local machine..
   provisioner "local-exec" {
     working_dir = "../Ansible"
     command = "ansible-playbook --inventory ${aws_instance.demo[0].public_ip}, --private-key ${var.ssh_key_private} --user ubuntu deploy-node.yaml"
